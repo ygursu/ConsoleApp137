677477e33a41df7a522385fd7141038eaa994054
ygursu <yasemingrs@gmail.com>
2024-03-12 23:09:16 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp137/Program.cs
@@ -0,0 +1,18 @@
+﻿double derece = 0;
+Console.Write("Sıcaklık değerini giriniz:");
+derece = Convert.ToDouble(Console.ReadLine());
+
+if (derece <= 0)
+{
+    Console.Write("KATI");
+}
+else if (derece > 0 && derece < 100)
+{
+    Console.Write("SIVI");
+}
+else
+{
+    Console.Write("GAZ");
+}
+
+Console.ReadKey();
